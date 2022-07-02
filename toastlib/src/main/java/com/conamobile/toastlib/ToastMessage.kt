f951c36982386cf77d37834f309b87179a9c04be
ConaMobile <conamobiledev@gmail.com>
2022-07-02 13:16:13 +0500

    add library

---
+++ toastlib/src/main/java/com/conamobile/toastlib/ToastMessage.kt
@@ -0,0 +1,10 @@
+package com.conamobile.toastlib
+
+import android.content.Context
+import android.widget.Toast
+
+object ToastMessage {
+    fun show(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+}
